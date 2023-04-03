cb06ec7980a6e2106bcef00cacf9389ed923d13a
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-04-03 14:03:50 +0700

    update rating page

--- src/pages/Rating/Rating.jsx
+++ src/pages/Rating/Rating.jsx
@@ -5,6 +5,12 @@ import {
   RiArrowGoBackFill,
   RiShoppingCart2Line,
 } from 'react-icons/ri'
+import { Space, Tag } from 'antd';
+
+
+const { CheckableTag } = Tag;
+const tagsData = ['Services', 'Quantity', 'Payment', 'Delivery', 'Promotion', 'Gift'];
+
 
 const Navbar = () => {
   return (
@@ -23,19 +29,42 @@ const Navbar = () => {
 const Rating = () => {
   const [review, setReview] = useState('');
   const [rating, setRating] = useState(0);
-
+  /*=====Review======*/
   const handleReviewChange = e => {
     setReview(e.target.value);
   }
-
+  /*=====Rating=====*/
   const handleRatingChange = value => {
     setRating(value);
   }
+  /*=====Tags=====*/
+  const [selectedTags, setSelectedTags] = useState(['Books']);
+  const handleChange = (tag, checked) => {
+    const nextSelectedTags = checked
+      ? [...selectedTags, tag]
+      : selectedTags.filter((t) => t !== tag);
+    console.log('You are interested in: ', nextSelectedTags);
+    setSelectedTags(nextSelectedTags);
+  };
 
   return (
     <div className='rating'>
       <Navbar />
       <div className='review-container'>
+        <div className='rating-tag'>
+          <h2>How do you feel about the service?</h2>
+          <Space size={[0, 8]} wrap>
+            {tagsData.map((tag) => (
+              <CheckableTag
+                key={tag}
+                checked={selectedTags.includes(tag)}
+                onChange={(checked) => handleChange(tag, checked)}
+              >
+                {tag}
+              </CheckableTag>
+            ))}
+          </Space>
+        </div>
         <div className='rating-scale'>
             <h3>Rating</h3>
             <div className='rating-star'>
