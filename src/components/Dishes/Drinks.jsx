82faa87b28f2b7320b000b1b593aa1261613c043
chanhtran2410 <tranletrungchanh12345@gmail.com>
2023-04-03 22:07:51 +0700

    update api add item

--- src/components/Dishes/Drinks.jsx
+++ src/components/Dishes/Drinks.jsx
@@ -1,42 +1,92 @@
-import React from 'react'
-import './Food.css'
-import { Link } from 'react-router-dom'
-import {RiDeleteBin6Line ,RiEdit2Line} from 'react-icons/ri'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import './Food.css'
+// import {RiDeleteBin6Line ,RiEdit2Line} from 'react-icons/ri'
 
-const Item =() =>{
+// const Item =() =>{
+//   return (
+//     <div className='item_block'>
+//         <div className='item_img'>
+//           <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
+//         </div>
+//         <div className='item_info'>
+//           <h3>Name</h3>
+//           <p>Description</p>
+//         </div>
+//         <div className='item_avai'>
+//           <h2>Available</h2>
+//         </div>
+//         <div className='item_price'>
+//           <div className='edit'>
+//             <Link><button><RiDeleteBin6Line/></button></Link>
+//             <Link to='/manager/edit'><button><RiEdit2Line/></button></Link>
+//           </div>
+//           <div className='price'>
+//             <h2>$100.000</h2>
+//           </div>
+//         </div>
+//       </div>
+//   )
+// }
+
+// const Food = () => {
+//   return (
+//     <div className='food'>
+//       <Item />
+//     </div>
+//   )
+// }
+
+// export default Food
+
+
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Food.css';
+import { RiDeleteBin6Line, RiEdit2Line } from 'react-icons/ri';
+
+const Item = ({ item }) => {
+  const availability = item.is_available ? 'Available' : 'Unavailable';
   return (
     <div className='item_block'>
-        <div className='item_img'>
-          <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
-        </div>
-        <div className='item_info'>
-          <h3>Name</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-        </div>
-        <div className='item_avai'>
-          <h2>Available</h2>
-        </div>
-        <div className='item_price'>
+      <div className='item_img'>
+        <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
+      </div>
+      <div className='item_info'>
+        <h3>{item.item_name}</h3>
+        <p>{item.description}</p>
+      </div>
+      <div className='item_avai'>
+        <h2>{availability}</h2>
+      </div>
+      <div className='item_price'>
         <div className='edit'>
-            <Link><button><RiDeleteBin6Line/></button></Link>
-            <Link to='/manager/edit'><button><RiEdit2Line/></button></Link>
-          </div>
-          <div className='price'>
-            <h2>$100.000</h2>
-          </div>
+          <Link><button><RiDeleteBin6Line/></button></Link>
+          <Link to={`/manager/edit/${item.id}`}><button><RiEdit2Line/></button></Link>
+        </div>
+        <div className='price'>
+          <h2>{`$${item.price}`}</h2>
         </div>
       </div>
-  )
-}
-
-const Drinks = () => {
-  return (
-    <div className='food'>
-      <Item />
-      <Item />
-      <Item />
     </div>
   )
 }
 
-export default Drinks
+const Drinks = () => {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:1500/api/menu')
+      .then(response => response.json())
+      .then(data => setMenuItems(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className='food'>
+      {menuItems.map(item => item.category=='drink' && <Item key={item.id} item={item} />)}
+    </div>
+  )
+}
+
+export default Drinks;
--- src/components/Dishes/Food.jsx
+++ src/components/Dishes/Food.jsx
@@ -1,42 +1,92 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './Food.css'
-import {RiDeleteBin6Line ,RiEdit2Line} from 'react-icons/ri'
+// import React from 'react'
+// import { Link } from 'react-router-dom'
+// import './Food.css'
+// import {RiDeleteBin6Line ,RiEdit2Line} from 'react-icons/ri'
 
-const Item =() =>{
+// const Item =() =>{
+//   return (
+//     <div className='item_block'>
+//         <div className='item_img'>
+//           <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
+//         </div>
+//         <div className='item_info'>
+//           <h3>Name</h3>
+//           <p>Description</p>
+//         </div>
+//         <div className='item_avai'>
+//           <h2>Available</h2>
+//         </div>
+//         <div className='item_price'>
+//           <div className='edit'>
+//             <Link><button><RiDeleteBin6Line/></button></Link>
+//             <Link to='/manager/edit'><button><RiEdit2Line/></button></Link>
+//           </div>
+//           <div className='price'>
+//             <h2>$100.000</h2>
+//           </div>
+//         </div>
+//       </div>
+//   )
+// }
+
+// const Food = () => {
+//   return (
+//     <div className='food'>
+//       <Item />
+//     </div>
+//   )
+// }
+
+// export default Food
+
+
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import './Food.css';
+import { RiDeleteBin6Line, RiEdit2Line } from 'react-icons/ri';
+
+const Item = ({ item }) => {
+  const availability = item.is_available ? 'Available' : 'Unavailable';
   return (
     <div className='item_block'>
-        <div className='item_img'>
-          <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
+      <div className='item_img'>
+        <img src="https://www.crazymasalafood.com/wp-content/images/2017/07/Chicken-Rice.jpg" alt="item_img" />
+      </div>
+      <div className='item_info'>
+        <h3>{item.item_name}</h3>
+        <p>{item.description}</p>
+      </div>
+      <div className='item_avai'>
+        <h2>{availability}</h2>
+      </div>
+      <div className='item_price'>
+        <div className='edit'>
+          <Link><button><RiDeleteBin6Line/></button></Link>
+          <Link to={`/manager/edit/${item.id}`}><button><RiEdit2Line/></button></Link>
         </div>
-        <div className='item_info'>
-          <h3>Name</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>
-        </div>
-        <div className='item_avai'>
-          <h2>Available</h2>
-        </div>
-        <div className='item_price'>
-          <div className='edit'>
-            <Link><button><RiDeleteBin6Line/></button></Link>
-            <Link to='/manager/edit'><button><RiEdit2Line/></button></Link>
-          </div>
-          <div className='price'>
-            <h2>$100.000</h2>
-          </div>
+        <div className='price'>
+          <h2>{`$${item.price}`}</h2>
         </div>
       </div>
-  )
-}
-
-const Food = () => {
-  return (
-    <div className='food'>
-      <Item />
-      <Item />
-      <Item />
     </div>
   )
 }
 
-export default Food
+const Food = () => {
+  const [menuItems, setMenuItems] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:1500/api/menu')
+      .then(response => response.json())
+      .then(data => setMenuItems(data))
+      .catch(error => console.log(error));
+  }, []);
+
+  return (
+    <div className='food'>
+      {menuItems.map(item => item.category=='food' && <Item key={item.id} item={item} />)}
+    </div>
+  )
+}
+
+export default Food;
