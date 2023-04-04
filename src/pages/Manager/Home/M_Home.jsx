f0244e579431990234fac59b066392f0cae233fe
solarmoonkid <khoa.trangtrong@hcmut.edu.vn>
2023-04-05 00:17:24 +0700

    edit style without blink

--- src/pages/Manager/Home/M_Home.jsx
+++ src/pages/Manager/Home/M_Home.jsx
@@ -1,17 +1,72 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import Sidebar from '../../../components/Sidebar/Sidebar'
 import MainContent from '../../../components/MainContent/MainContent'
 import Table from "./Table"
-import {useState, useEffect} from 'react'
+import { Tabs } from 'antd';
+// import {
+//     StyleSheet,
+//     Text,
+//     SafeAreaView,
+//     ScrollView,
+//     StatusBar,
+//   } from 'react-native';
 import "./M_Home.css"
 
 
 
-const tableStatus = [[1,true, false], [2,true, false], [3,false, false], [4,false, false], [5,false,false], [6,true,false], [7,true,false], [8,false,false], [9,true, true], [10,true, true], [11,false, false], [12,true, false], [13,false,false], [14,false,false], [15,true,false], [16,false,false]]
+// const tableStatus = [[1,true, false], [2,true, false], [3,false, false], [4,false, false], [5,false,false], [6,true,false], [7,true,false], [8,false,false], [9,true, true], [10,true, true], [11,false, false], [12,true, false], [13,false,false], [14,false,false], [15,true,false], [16,false,false]]
+
+const tableStatus = [[1, 0], [2, 0], [3, 2], [4, 3], [5, 2], [6, 3], [7, 0], [8, 2], [9, 0], [10, 2], [11, 3], [12, 3], [13, 1], [14, 2], [15, 0], [16, 2]]
 
 const orderComponent = [[["Dish1", 100000], ["Dish2", 100000]],[["Dish3", 100000], ["Dish4", 100000], ["Dish5", 100000]]]
 
 const tip = 10
+
+/* API Stuff
+
+const addItem = async () => {
+    if(localStorage.getItem("user")){
+      const config = {
+        headers: {
+          Authorization: "Bearer " + JSON.parse(localStorage.getItem("user")).access_token,
+        },
+      };
+    
+    try {
+      const data = {
+        item_name: name,
+        description:description,
+        price:price,
+        is_available: availability,
+        category:category,
+      };
+      console.log(data);
+      await axios.post("http://localhost:1500/api/add-item", data, config);
+      message.success("Item added successfully");
+    } catch (error) {
+      console.error(error);
+      message.error("Error adding item");
+    }
+  }
+  };
+
+
+  const config = {
+    headers: {
+      Authorization:
+        "Bearer " +
+        JSON.parse(localStorage.getItem("user")).access_token,
+    },
+  };
+
+  useEffect(() => {
+    fetch("http://localhost:1500/api/menu")
+      .then((response) => response.json())
+      .then((data) => setMenuItems(data))
+      .catch((error) => console.log(error));
+  }, []);
+*/
+
 
 const DishPrice = (props) => {
     return(
@@ -29,6 +84,7 @@ const DishPrice = (props) => {
 const TableSelect = () =>{
     const [selection, setSelection] = useState(0);
     const [isToggled, toggle] = useState(false)
+    const [statusDisplay, setStatusDisplay] = useState(0)
     let displayB = false;
     if (selection < 1) {displayB = false}
     else {displayB = true}
@@ -55,9 +111,9 @@ const TableSelect = () =>{
         <div className="tableSelect">
             <h1 className='title'>TABLES</h1>
             {tableStatus.map((Tstatus) => {
-                return <Table idn={Tstatus[0]} free={Tstatus[1]} paying={Tstatus[2]} setSel={setSelection}/>
+                return <Table idn={Tstatus[0]} free={Tstatus[1]} setSel={setSelection} setTog={toggle} setStat={setStatusDisplay}/>
             })}
-            {displayB && <TableContent tablenumber={selection} turnoff={turnoff} tog={isToggled} togf={toggle} checkout={checkout} paying={tableStatus[selection][1]}/>}
+            {displayB && <TableContent tablenumber={selection} turnoff={turnoff} tog={isToggled} togf={toggle} checkout={checkout} paying={tableStatus[selection-1][1]} statusD={statusDisplay}/>}
         </div>
     )
 }
@@ -72,7 +128,7 @@ const Toggle = ({ label, tog, togf, onClick, turnoff }) => {
 
     return (
         <label>
-            <input type="checkbox" defaultChecked={tog} onClick={callback} />
+            <input type="checkbox" defaultChecked={false} checked={tog} onClick={callback} />
             <span />
             <strong>{label}</strong>
         </label>
@@ -80,41 +136,47 @@ const Toggle = ({ label, tog, togf, onClick, turnoff }) => {
 
 const TableContent = (props) =>{
 
-    const logState = state => {
-        console.log("Toggled:", state)
-        props.togf(!props.tog)
-    }
+    // const logState = state => {
+    //     console.log("Toggled:", state)
+    //     // props.togf(!props.tog)
+    // }
 
     let total = 0
+    let tipD = (props.statusDisplay == 3)
     orderComponent[props.tablenumber%2].map((dish) => {
         total = total + dish[1]})
     total = total + tip
+    // console.log(props.tog)
 
     return(
         <div className="tableContent">
-            <h1 id="tablelab">Table {props.tablenumber}</h1>
+            <h1 id="tablelab">Table {props.tablenumber} </h1>
             <h3 className='stext'>Checkin time: 12:34:56</h3>
             <h1 id="orderlab">Orders</h1>
             <Toggle className="Togglebutton"
             label=""
             // toggled={props.tog}
-            onClick={logState}
+            onClick={props.togf}
             turnoff={props.turnoff}
             tog={props.tog}
             togf={props.togf}
             />
           {props.tog && <div id="billContainer">
-                {/* <ScrollView> */}
+                {/* <ScrollArea> */}
                 {orderComponent[props.tablenumber%2].map((dish) => {
                     return <DishPrice className="bstextillTitle" name={dish[0]} price={dish[1]}/>
                 })}
-                {/* </ScrollView> */}
-            <DishPrice className="billTitle" name={"Tip"} price={tip}/>
-            <hr/>
-            <DishPrice className="billTitle" name={"Total"} price={total}/>
-            <div id="checkoutContainer">
-                <button id="checkoutButton" onClick={props.checkout}>Checkout</button>
-            </div>
+                {/* </ScrollArea> */}
+            {(props.statusD == 3) && (
+            <div>
+                <DishPrice className="billTitle" name={"Tip"} price={tip}/>
+                <hr/>
+                <DishPrice className="billTitle" name={"Total"} price={total}/>
+            
+                <div id="checkoutContainer">
+                    <button id="checkoutButton" onClick={props.checkout}>Checkout</button>
+                </div>
+            </div>)}
             </div>}
         </div>
     )
--- src/pages/Manager/Home/Table.jsx
+++ src/pages/Manager/Home/Table.jsx
@@ -2,9 +2,9 @@ import React, {useState, useEffect} from "react"
 
 const Table = (props) =>{
   const Cwhite = "white"
-  const Corange = "rgba(255, 200, 50,0.6)"
-  const Cyellow = "rgba(255, 200, 50,0.6)"
-  const Cgreen = "rgba(255, 200, 50,0.6)"
+  const Corange = "rgba(255, 200, 50, 0.6)"
+  const Cyellow = "rgba(255, 255, 0, 0.5)"
+  const Cgreen = "rgba(0, 255, 0, 0.5)"
 
   const [color, setColor] = useState("white")
   const [free, setFree] = useState(props.free)
@@ -13,6 +13,14 @@ const Table = (props) =>{
 
   const handleClick = () => {
     props.setSel(props.idn)
+    if (props.free == 0 || props.free == 1) props.setTog(false)
+    else props.setTog(true)
+    props.setStat(props.free)
+
+    if (props.free == 0) setColor(Cwhite)
+    else if (props.free == 1) setColor(Cyellow)
+    else if (props.free == 2) setColor(Corange)
+    else if (props.free == 3) setColor(Cgreen)
     // if (free == true) {setColor(Corange);}
     // else if (paying == false) {setColor(Cgreen);}
     // else {setColor(Cwhite);}
@@ -25,11 +33,16 @@ const Table = (props) =>{
 
   useEffect(() => {
     // setFree(props.free)
-    if (free == false) setColor(Corange)
-    else {
-      if (paying == false) setColor(Cgreen)
-      setColor(Cwhite)
-    }
+    // if (free == false) setColor(Corange)
+    // else {
+    //   if (paying == false) setColor(Cgreen)
+    //   setColor(Cwhite)
+    // }
+    console.log(props.free)
+    if (props.free == 0) setColor(Cwhite)
+    else if (props.free == 1) setColor(Cyellow)
+    else if (props.free == 2) setColor(Corange)
+    else if (props.free == 3) setColor(Cgreen)
     // props.setSel(props.idn, props.free)
   }, [])
 
@@ -47,6 +60,18 @@ const Table = (props) =>{
     left: (((props.idn-1)%4)*140 + 80) + 'px',
     boxShadow: '5px 10px 18px #acacac',
   }
+
+  // {
+  //   animation:blinking 1.5s infinite;
+  //   font-size: 20px;
+  //   }
+  //   @keyframes blinking{
+  //   0%{   color: red;   }
+  //   47%{   color: #000; }
+  //   62%{   color: transparent; }
+  //   97%{   color:transparent; }
+  //   100%{  color: #000;   }
+  //   }
 
     return(
         <div className="Table">
